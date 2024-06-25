f012163a92adff6121f060e27999ebf7ad248cd3
sebrz <h.sebastian.ruiz@gmail.com>
2024-06-25 14:58:44 -0500

    SSET

---
+++ SSET_Counting_Subsets.py
@@ -0,0 +1,12 @@
+# Given a positive integer n <= 10000
+# Return the total number of subsets modulo 1,000,000
+import math
+
+if __name__ == '__main__':
+
+    myfile = input('Enter file name: ')
+    myfile = 'Data_files/' + myfile + '.txt'
+    f = open(myfile, "r")
+    n = int(f.readline().strip())
+    sol = 2 ** n
+    print(sol % 1000000)
